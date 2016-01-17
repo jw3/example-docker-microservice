99a6ea699b4b6940e6e287ea017bccff4a7ece58
John Wass <jwass3@gmail.com>
2016-01-16 23:42:51 -0500

    twitter to kafka example - upgraded to akka streams 2.0 - requires awebapi 0.3

--- build.sbt
+++ build.sbt
@@ -9,17 +9,18 @@ scalaVersion := "2.11.7"
 scalacOptions += "-target:jvm-1.8"
 
 libraryDependencies ++= {
-    val akkaVersion = "2.4.0"
-    val akkaStreamVersion = "1.0"
+    val akkaVersion = "2.4.1"
+    val akkaStreamVersion = "2.0.1"
 
     Seq(
-        "wiii" %% "awebapi" % "0.2",
+        "wiii" %% "awebapi" % "0.3",
 
         "io.spray" %% "spray-json" % "1.3.2",
         "com.typesafe" % "config" % "1.3.0",
         "net.ceedubs" %% "ficus" % "1.1.2",
         "org.twitter4j" % "twitter4j-core" % "4.0.4",
         "org.twitter4j" % "twitter4j-stream" % "4.0.4",
+        "com.softwaremill.reactivekafka" %% "reactive-kafka-core" % "0.9.0",
 
         "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0",
         "ch.qos.logback" % "logback-classic" % "1.1.3",
---
+++ src/main/scala/kafka/feedread/FeedToKafka.scala
@@ -0,0 +1,87 @@
+package kafka.feedread
+
+import java.util
+
+import akka.actor._
+import akka.stream.actor.ActorSubscriber
+import akka.stream.scaladsl.{Keep, Sink, Source}
+import akka.stream.{ActorMaterializer, OverflowStrategy}
+import com.softwaremill.react.kafka.{ProducerMessage, ProducerProperties, ReactiveKafka}
+import com.typesafe.scalalogging.LazyLogging
+import kafka.feedread.PrivateFeedToKafka._
+import org.apache.kafka.common.serialization.{Deserializer, Serializer}
+import spray.json.{DefaultJsonProtocol, _}
+import twitter4j.{Status, _}
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+/**
+ * twitter to kafka microservice example
+ * todo;; externalize configuration
+ */
+object FeedToKafka extends App with LazyLogging {
+    implicit val actorSystem: ActorSystem = ActorSystem("FeedToKafka")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+    val topicName = "test_topic"
+
+    val subscriberProps = new ReactiveKafka().producerActorProps(ProducerProperties(
+        bootstrapServers = "localhost:9092",
+        topic = topicName,
+        valueSerializer = TweetSerializer
+    ))
+    val subscriber = actorSystem.actorOf(subscriberProps)
+    val (actorRef, publisher) = Source.actorRef[Status](1000, OverflowStrategy.fail).toMat(Sink.asPublisher(false))(Keep.both).run()
+
+    val factory = new TwitterStreamFactory()
+    val twitterStream = factory.getInstance()
+    twitterStream.addListener(new StatusForwarder(actorRef))
+    twitterStream.filter(new FilterQuery("espn"))
+
+    Source.fromPublisher(publisher).map(s => ProducerMessage(Tweet(s.getUser.getName, s.getText)))
+    .runWith(Sink.fromSubscriber(ActorSubscriber[ProducerMessage[Array[Byte], Tweet]](subscriber)))
+
+    Await.ready(actorSystem.whenTerminated, Duration.Inf)
+    // read a twitter feed writing to kafka topic
+}
+
+class StatusForwarder(publisher: ActorRef) extends StatusListener {
+    def onStatus(status: Status): Unit = publisher ! status
+
+    //\\ nop all the others for now  //\\
+    def onStallWarning(warning: StallWarning): Unit = {}
+    def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice): Unit = {}
+    def onScrubGeo(userId: Long, upToStatusId: Long): Unit = {}
+    def onTrackLimitationNotice(numberOfLimitedStatuses: Int): Unit = {}
+    def onException(ex: Exception): Unit = {}
+}
+
+private[feedread] object PrivateFeedToKafka {
+    case class Tweet(user: String, text: String)
+
+    object TweetProtocol extends DefaultJsonProtocol {
+        implicit val currencyRteFormat = jsonFormat2(Tweet)
+    }
+
+    object TweetSerializer extends Serializer[Tweet] {
+        import kafka.feedread.PrivateFeedToKafka.TweetProtocol._
+
+        override def serialize(s: String, t: Tweet): Array[Byte] =
+            t.toJson.compactPrint.getBytes("UTF-8")
+
+        override def close(): Unit = {}
+        override def configure(map: util.Map[String, _], b: Boolean): Unit = {}
+    }
+
+    object TweetDeserializer extends Deserializer[Tweet] {
+        import kafka.feedread.PrivateFeedToKafka.TweetProtocol._
+
+        override def deserialize(s: String, bytes: Array[Byte]): Tweet =
+            new String(bytes, "UTF-8").parseJson.convertTo[Tweet]
+
+
+        override def close(): Unit = {}
+        override def configure(map: util.Map[String, _], b: Boolean): Unit = {}
+    }
+}
